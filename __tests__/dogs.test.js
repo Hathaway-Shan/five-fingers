beec8355cf8dc16cc826c2ae5b94315fba230b49
Hathaway-Shan <hathaway.shan@gmail.com>
2022-08-04 12:35:57 -0700

    wrote test for get all dogs

--- __tests__/dogs.test.js
+++ __tests__/dogs.test.js
@@ -1,14 +1,15 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('#get dogs returns a list of dog', async () => {
+    const res = await request(app).get('/dogs');
+    expect(res.body.length).toEqual(3);
   });
   afterAll(() => {
     pool.end();
