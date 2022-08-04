8727400c5b19934733b14b8717d0b8a30943291b
Hathaway-Shan <hathaway.shan@gmail.com>
2022-08-04 12:22:03 -0700

    wrote test for get all magic cards

--- __tests__/cards.test.js
+++ __tests__/cards.test.js
@@ -1,14 +1,15 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('#get magic_cards returns a list of magic cards', async () => {
+    const res = await request(app).get('/magic_cards');
+    expect(res.body.length).toEqual(3);
   });
   afterAll(() => {
     pool.end();
