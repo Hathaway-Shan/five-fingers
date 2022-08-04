38afcd0ea7041e8f5a375d6cdcfdb0400b2ba80b
Hathaway-Shan <hathaway.shan@gmail.com>
2022-08-04 16:11:03 -0700

    wrote test for getting foods by id

--- __tests__/food.test.js
+++ __tests__/food.test.js
@@ -11,6 +11,15 @@ describe('backend-express-template routes', () => {
     const res = await request(app).get('/food');
     expect(res.body.length).toEqual(3);
   });
+  it('#get foods by id returns first row', async () => {
+    const res = await request(app).get('/foods/1');
+    const firstEntity = {
+      id: '1',
+      name: 'Pizza',
+      breed: 'Italian',
+    };
+    expect(res.body).toEqual(firstEntity);
+  });
   afterAll(() => {
     pool.end();
   });
